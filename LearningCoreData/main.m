b1dea88e514f4b9be7b3e1ad2f3f2a35f41591fd
panaah <vijayinder@superintech.com>
2015-02-04 08:26:42 +0530

    Initial Commit

---
+++ LearningCoreData/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LearningCoreData
+//
+//  Created by Thakur VJ on 04/02/15.
+//  Copyright (c) 2015 Thakur VJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
